90080afbbec5a49fd5c4ea3541d7aff086363840
김민태 <kimmintae@gimmintaeui-MacBookPro.local>
2024-08-14 11:42:45 +0900

    home route add

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,10 @@ const router = createBrowserRouter([
     {
         path: "/:id",
         element: <BusinessCard/>,
+    },
+    {
+      path: "/",
+      element: <BusinessCard/>,
     }
 ])
 
